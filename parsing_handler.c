88b4eaf552b703764848f079362152d5ff98cc0c
Huzaifah Ahmad <123182931+HuzaifahAhmad@users.noreply.github.com>
2024-10-04 13:28:42 -0400

    working on parser

--- parsing_handler.c
+++ parsing_handler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "header.h"
 
@@ -8,16 +9,28 @@ void parsing_handler(char input_string[])
 {
     char delimiter[] = " ";
     char *p_first_tokenized_string;
-    char parsed_string[1000];
-    // char pipe_symbol[] = "|";
+    char **parsed_string = NULL;
+    int index = 0, size = 1;
+
+    parsed_string = malloc(1 * sizeof(char*));
 
     p_first_tokenized_string = strtok(input_string, delimiter);
 
     while (p_first_tokenized_string != NULL) 
     {
-        printf("%s\n", p_first_tokenized_string);
+        parsed_string = realloc(parsed_string, size * sizeof(char*));
+        parsed_string[index] = p_first_tokenized_string;
+
+        size++;
+        index++;
+
         p_first_tokenized_string = strtok(NULL, delimiter);
-
-
     }
+
+    for (int i = 0; i < size - 1; i++) 
+    {
+        printf("%s\n", parsed_string[i]);
+    }
+
+    free(parsed_string); 
 }
