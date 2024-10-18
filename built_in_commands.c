88f287170844fd6567d2151730050e13071db370
Huzaifah Ahmad <123182931+HuzaifahAhmad@users.noreply.github.com>
2024-10-18 13:05:25 -0400

    implemented history command

--- built_in_commands.c
+++ built_in_commands.c
@@ -9,7 +9,7 @@
 
 void execute_built_in_command(char **built_in_command_execution) 
 {
-    char cwd[1024];
+    printf("\nMADE IT TO BUILTIN COMMANDS\n");
 
     for (int i = 0; built_in_command_execution[i] != NULL; i++) 
     {
@@ -17,17 +17,18 @@ void execute_built_in_command(char **built_in_command_execution)
         // pwd command implementation
         if (strcmp(built_in_command_execution[0], "pwd") == 0)
         {
+            char cwd[1024];
+
             if (getcwd(cwd, sizeof(cwd)) != NULL) {
                 printf("%s\n", cwd);  // print current directory
             } else {
                 perror("getcwd() error");  // handle error
             }
-            break;
         }
 
 
         // cd command implementation
-        else if (strcmp(built_in_command_execution[0], "cd") == 0)
+        if (strcmp(built_in_command_execution[0], "cd") == 0)
         {
             chdir(built_in_command_execution[i + 1]);
             printf("directory changed\n");
@@ -36,7 +37,7 @@ void execute_built_in_command(char **built_in_command_execution)
 
 
         // echo command implementation
-        else if (strcmp(built_in_command_execution[0], "echo") == 0)
+        if (strcmp(built_in_command_execution[0], "echo") == 0)
         {
             for (int j = 1; built_in_command_execution[j] != NULL; j++)
             {
@@ -46,12 +47,19 @@ void execute_built_in_command(char **built_in_command_execution)
             break;
         }
 
-        // command not found implementation
-        else 
+        else if (strcmp(built_in_command_execution[0], "history") == 0)
         {
-            printf("huz-shell: Command not found\n");
-            break;
+            printf("HISTORY\n");
         }
+
+
+
+        // command not found implementation
+        // else 
+        // {
+        //     printf("huz-shell: Command not found\n");
+        //     break;
+        // }
 
      }
 }
--- main.c
+++ main.c
@@ -2,24 +2,49 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include "header.h"
 #include "header.h"
 
 #define MAX_LIMIT 1000
 
+// function to trim leading and trailing spaces
+char *trim_whitespace(char *str) {
+    char *end;
+
+    // trim leading space
+    while(isspace((unsigned char)*str)) str++;
+
+    // If all spaces
+    if(*str == 0)  
+        return str;
+
+    // Trim trailing space
+    end = str + strlen(str) - 1;
+    while(end > str && isspace((unsigned char)*end)) end--;
+
+    // Write new null terminator
+    *(end+1) = 0;
+
+    return str;
+}
 
 int main() {
     char get_command_from_user[MAX_LIMIT];
     char exit[] = "exit";
-    char *built_in_commands_strings[] = {
-        "pwd",
-        "cd",
-        "echo",
-        "history"
-    };
+    char *built_in_commands_strings[] = 
+    { "cd", "pwd", "echo", "history"};
 
     int num_of_built_in_commands = sizeof(built_in_commands_strings) / sizeof(char *);
+    int history_count = 0;
+    int history_array_size = 10;
+
+    char **history_of_commands = malloc(10 * sizeof(char*));
+    if (history_of_commands == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
 
     printf("START OF SHELL\n");
     
@@ -37,6 +62,45 @@ int main() {
             break;
         }
 
+        char *trimmed_command = trim_whitespace(get_command_from_user);
+
+        // check if history array needs to be expanded
+        if (history_count >= history_array_size) {
+            // double the size of the history array
+            history_array_size += 10;
+            char **temp = realloc(history_of_commands, history_array_size * sizeof(char*));
+            if (temp == NULL) {
+                printf("Reallocation failed\n");
+                return 1;
+            }
+            history_of_commands = temp;  // point to the new, resized memory
+        }
+
+        // add the command to history
+        history_of_commands[history_count] = malloc(strlen(get_command_from_user) + 1);
+        if (history_of_commands[history_count] == NULL) {
+            printf("Memory allocation for command failed\n");
+            return 1;
+        }
+
+        // check if the command is empty or consists of only spaces
+        if (strlen(trimmed_command) == 0) 
+        {
+        } 
+        else {
+            // accept and store the command
+            strcpy(history_of_commands[history_count], trimmed_command);
+            history_count++;
+        }
+
+        // print the current history
+        if (strcmp(get_command_from_user, "history") == 0) 
+        {
+            for (int i = 0; i < history_count; i++) {
+                printf("%s\n", history_of_commands[i]);
+            }
+        }
+
 
         // check for any wildcard characters in the input
         char *result_from_wildcard_globbing_funtion = wildcard_globbing(get_command_from_user);
@@ -47,10 +111,8 @@ int main() {
         // parsing after expanding wildcard character
         char **parsed_command = parsing_handler(get_command_from_user);
 
-        // piping handler - checks for pipes and handles accordingly
-        // piping_handler(parsed_command);
 
-
+        // checks if input is not null, then checks if the in the input is a builtin or external command and executes accordingly
         if (parsed_command[0] != NULL) 
         {
             for (int i = 0; i < num_of_built_in_commands; i++) 
@@ -71,9 +133,15 @@ int main() {
 
 
 
-        free(parsed_command); 
-    }
 
+    free(parsed_command);
+    }
+    
+    for (int i = 0; i < history_count; i++) {
+        free(history_of_commands[i]);
+    }
+    free(history_of_commands);  
+    
     printf("\n");
     printf("THE SHELL HAS TERMINATED!");
 
